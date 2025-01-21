3a2b8288ecb12cc85bf8048da5959a75bdcd117d
jean-claude <jean-claude.grech@orange.fr>
2025-01-21 18:50:42 +0100

    insert makay

--- src/pages/NewBoats.tsx
+++ src/pages/NewBoats.tsx
@@ -6,6 +6,9 @@ import imageMirage18 from "../images/mirage_18.jpg";
 import odyssey25 from "../images/odyssey-25.jpg";
 import mirageAtSea from "../images/mirage-at-sea.jpg";
 import odysseyAtSea from "../images/odyssey-at-sea.jpg"
+import makayAnchored from "../images/makay-anchored.jpeg";
+import makayBabord from "../images/makay-babord.jpeg";
+import makayCockpitRs from "../images/makay-cockpit-rs.jpeg";
 
 const NewBoats: React.FC = () => {
 	const newCards = [
@@ -43,8 +46,24 @@ const NewBoats: React.FC = () => {
 			description:
 				"Designed by STAMA Design, the ODYSSEY 28 stands out for its sleek appearance and elegant finishes.",
 		},
-
-	
+		{
+			title: "Makay",
+			image: makayAnchored,
+			description:
+				"The MAKAI M37 is designed to seamlessly blend luxury and performance, setting a new standard for powercats. Drawing inspiration from the timeless elegance and high performance of sports cars.",
+		},
+		{
+			title: "Port-side cabin",
+			image: makayBabord,
+			description:
+				"The M37 motor catamaran is configured with two cabins: a master cabin and a guest cabin.",
+		},
+		{
+			title: "Makay Cockpit",
+			image: makayCockpitRs,
+			description:
+				"The incredibly spacious cockpit area is among the largest in its class, setting a new standard for this model and boat size",
+		},
 	];
 	return (
 		<div className={Styles.newCardsContainer}>
